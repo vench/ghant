90a84c9e1ef91539190b14aa7e125ae95a787527
vench <nowwrit@mail.ru>
2014-10-15 09:49:37 +0400

    Update UserController.php
    Fix access

--- protected/controllers/UserController.php
+++ protected/controllers/UserController.php
@@ -37,7 +37,7 @@ class UserController extends Controller
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
 				'actions'=>array('admin','delete'),
-				'expression' => array('UserController','allowOnlyAdmin'),
+				'expression' => array($this,'allowOnlyAdmin'),
 			),
 			array('deny',  // deny all users
 				'users'=>array('*'),
