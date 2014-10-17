095e8330448c57b0aeb031658bf27a6c98980866
vench <nowwrit@mail.ru>
2014-10-17 22:44:08 +0400

    fix calendar

--- protected/controllers/ProjectController.php
+++ protected/controllers/ProjectController.php
@@ -99,7 +99,7 @@ class ProjectController extends Controller
                 $data[] = array(
                     'title'=>$task->description,
                     'start'=>strtotime($task->start_date),
-                    'end'=>strtotime($task->end_date)-1,
+                    'end'=>strtotime($task->end_date),
                     'url'=>$this->createUrl('/project/view', array('id'=>$task->project_id)), 
                 );
             }
